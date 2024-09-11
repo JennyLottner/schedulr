9f914e1e829c6059dab7a8045cef02217c9a7cba
JennyLottner <jenny.lottner@gmail.com>
2024-09-11 12:50:46 +0300

    change header to login header

--- frontend/src/cmps/AppHeader.vue
+++ frontend/src/cmps/AppHeader.vue
@@ -1,34 +1,13 @@
 <template>
-<header>
-  <img class="account-logo" src="https://cdn.monday.com/images/logos/monday_logo_short.png" alt="Monday logo">
-</header>
+  <h1>header</h1>
 </template>
 
 <script>
 export default {
-  // computed: {
-  //   computedClass() {
 
-  //   }
-  // }
 }
 </script>
 
 <style lang="scss">
-header {
-  background-color: #f7f7f7;
-  // height: rem(65px);
-  height: 65px;
-  display: flex;
-  padding-inline-start: 20px;
-  border-bottom: 1px solid #e0e0e0;
-
-  img {
-    justify-self: self-start;
-    align-self: center;
-    width: 180px;
-    height: 44px;
-  }
-}
 
 </style>
---
+++ frontend/src/cmps/LoginHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <img class="account-logo" src="https://cdn.monday.com/images/logos/monday_logo_short.png" alt="Monday logo"/>
+  </header>
+</template>
+    
+<script>
+  export default {
+  // computed: {
+  //   computedClass() {
+
+  //   }
+  // }
+}
+</script>
+    
+<style lang="scss">
+  header {
+    background-color: #f7f7f7;
+    // height: rem(65px);
+    height: 65px;
+    display: flex;
+    padding-inline-start: 20px;
+    border-bottom: 1px solid #e0e0e0;
+  
+    img {
+      justify-self: self-start;
+      align-self: center;
+      width: 180px;
+      height: 44px;
+    }
+  }
+</style>
