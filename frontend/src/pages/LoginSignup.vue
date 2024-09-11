63c65289222772a918a4b7d6deecf0532040648f
JennyLottner <jenny.lottner@gmail.com>
2024-09-11 12:51:14 +0300

    Add login page

--- frontend/src/pages/LoginSignup.vue
+++ frontend/src/pages/LoginSignup.vue
@@ -1,13 +1,25 @@
 <template>
-  
+  <section>
+    <LoginHeader />
+    <main class="flex column center">
+      <h1>Log in to your account</h1>
+      <p>Enter your work email address</p>
+      <input type="email">
+      <button>Next</button>
+      <p>Or Sign in with</p>
+      <button>Google</button>
+      <p>Don't have an account yet? <RouterLink to="/signup">Sign up</RouterLink></p>
+      <p>Can't log in? <RouterLink to="/help-center">Visit our help center</RouterLink></p>
+    </main>
+  </section>
 </template>
 
 <script>
+import LoginHeader from '@/cmps/LoginHeader.vue';
 export default {
-
+  components: { LoginHeader }
 }
 </script>
 
-<style>
-
+<style lang="scss">
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from '../pages/Homepage.vue'
 import Main from '../pages/Main.vue'
+import LoginSignup from '@/pages/LoginSignup.vue'
+import Homepage from '../pages/Homepage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,12 @@ const router = createRouter({
           component: Homepage
         },
       ],
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginSignup,
+    },
   ]
 })
 
