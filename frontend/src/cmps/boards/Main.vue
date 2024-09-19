270f9fc14722372ebaca302651ab5d7bf38e017a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-20 02:45:25 +0300

    Move main to cmps dir to the boards dir

---
+++ frontend/src/cmps/boards/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="home-page">
+    <h1>home</h1>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.home-page {
+  height: 100%;
+  padding: 0.75rem;
+  border-radius: 0.5rem 0 0 0;
+  background-color: white;
+}
+</style>
