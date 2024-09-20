13f52d589c99c545b4cc704d31ef7cbe9e52f33f
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-20 04:09:07 +0300

    Create divider component in helpers dir

---
+++ frontend/src/cmps/helpers/Divider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '../style/setup/vars';
+
+.divider {
+  height: 1px;
+  background-color: $app-gray;
+}
+</style>
