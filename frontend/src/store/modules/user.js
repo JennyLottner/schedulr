c702ad9b0857bae58f8bb9ef5aa5e4acc2e87624
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-23 23:42:20 +0300

    Edit function in store

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -6,13 +6,8 @@ export default {
     state: {
         user: null
     },
-    getters: {
-        user(state) {
-            return state.user
-        }
-    },
     mutations: {
-        setUser(state, { user }) {
+        setUser(state, user) {
             state.user = user
         }
     },
