97d8893a7d494279c682a8e8cc405997ee203a2e
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-09-30 23:34:39 +0300

    Edit img urls in user local service

--- frontend/src/services/user.local.service.js
+++ frontend/src/services/user.local.service.js
@@ -7,21 +7,21 @@ let users = [
     {
         username: 'jenny',
         email: 'jenny@gmail.com',        
-        imgUrl: '../assets/profile-pics/jenny.png',
+        imgUrl: 'https://scontent.fsdv1-2.fna.fbcdn.net/v/t39.30808-1/336327249_1399754383903354_5431621484366514305_n.jpg?stp=dst-jpg_s480x480&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=7529rO5u1tAQ7kNvgEEoKSN&_nc_ht=scontent.fsdv1-2.fna&_nc_gid=AJMWynPri4HlHr79AxdDNqP&oh=00_AYBNuYj9-SgXl13ix9rrRHMcgWBFeaQqgUTQt_k2LyJZ_A&oe=6700CD62',
         password: 'manoon',
         activities: []
     },
     {
         username: 'shoval',
         email: 'shoval@gmail.com',
-        imgUrl: '../assets/profile-pics/shoval.png',
+        imgUrl: 'https://scontent.fsdv1-2.fna.fbcdn.net/v/t39.30808-6/409621629_3462157944095507_399315322993000288_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=AAjr-5x7C3oQ7kNvgGPEnb0&_nc_ht=scontent.fsdv1-2.fna&_nc_gid=AEZK1n0-pFOwR4VzGziaVgR&oh=00_AYCo6eHPoOilPHLotIoAeEeQZHRUE7_GeiIzNEQG44WnEg&oe=6700D3AC',
         password: 'manoon',
         activities: []
     },
     {
         username: 'guest',
         email: 'guest@gmail.com',
-        imgUrl: '../assets/profile-pics/guest.png',
+        imgUrl: 'https://scontent.fsdv1-2.fna.fbcdn.net/v/t39.30808-6/450085430_3609207502723883_1972484456701139373_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xVqB4dO96xMQ7kNvgHDFRCp&_nc_ht=scontent.fsdv1-2.fna&oh=00_AYD_5Iw53ksjJcK1RoZaBfaOiXv8dr6IybOBJD7hDFPIOQ&oe=6700F8D8',
         password: 'guest',
         activities: []
     }
