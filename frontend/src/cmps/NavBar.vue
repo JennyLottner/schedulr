8f21186d3ccee6263bc827cb52f42fa252f5bc2a
JennyLottner <jenny.lottner@gmail.com>
2025-01-15 16:43:03 +0200

    Remove irrelevant nav-bar tabs

--- frontend/src/cmps/NavBar.vue
+++ frontend/src/cmps/NavBar.vue
@@ -1,39 +1,48 @@
 <template>
   <nav class="flex column">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink>My work</RouterLink>
+    <RouterLink to="/">
+      <svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16" aria-hidden="true" class="icon_90dcd5fe7a egsFo" data-testid="icon"><path d="M9.56992 2.1408C9.82591 1.95307 10.1741 1.95307 10.4301 2.1408L17.7028 7.47413C17.8896 7.61113 18 7.82894 18 8.06061V16.7879C18 17.1895 17.6744 17.5152 17.2727 17.5152H11.9394C11.5377 17.5152 11.2121 17.1895 11.2121 16.7879V13.1515H8.78788V16.7879C8.78788 17.1895 8.46227 17.5152 8.06061 17.5152H2.72727C2.32561 17.5152 2 17.1895 2 16.7879V8.06061C2 7.82894 2.11037 7.61113 2.29719 7.47413L9.56992 2.1408ZM3.45455 8.42914V16.0606H7.33333V12.4242C7.33333 12.0226 7.65894 11.697 8.06061 11.697H11.9394C12.3411 11.697 12.6667 12.0226 12.6667 12.4242V16.0606H16.5455V8.42914L10 3.62914L3.45455 8.42914Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>  </svg>
+      Home
+    </RouterLink>
+
+    <RouterLink>
+      <svg viewBox="0 0 20 20" fill="currentColor" width="18" height="18" aria-hidden="true" tabindex="-1" class="icon_a812034417 st-current" data-testid="icon"><path d="M5.99986 1.82129C6.41407 1.82129 6.74986 2.15708 6.74986 2.57129V4.10701H13.2499V2.57129C13.2499 2.15708 13.5856 1.82129 13.9999 1.82129C14.4141 1.82129 14.7499 2.15708 14.7499 2.57129V4.107H16.2856C16.7876 4.107 17.269 4.30643 17.624 4.66141C17.979 5.01639 18.1784 5.49784 18.1784 5.99986V16.2856C18.1784 16.7876 17.979 17.269 17.624 17.624C17.269 17.979 16.7876 18.1784 16.2856 18.1784H3.71415C3.21213 18.1784 2.73067 17.979 2.37569 17.624C2.02071 17.269 1.82129 16.7876 1.82129 16.2856V5.99986C1.82129 5.49784 2.02071 5.01639 2.37569 4.66141C2.73067 4.30643 3.21213 4.107 3.71415 4.107C3.763 4.107 3.81077 4.11168 3.85702 4.1206C3.90326 4.11168 3.95102 4.10701 3.99986 4.10701H5.24986V2.57129C5.24986 2.15708 5.58565 1.82129 5.99986 1.82129ZM5.24986 7.14272V5.60701H3.99986C3.95101 5.60701 3.90324 5.60234 3.85699 5.59342C3.81075 5.60233 3.76299 5.607 3.71415 5.607C3.60995 5.607 3.51003 5.64839 3.43635 5.72207C3.36268 5.79574 3.32129 5.89567 3.32129 5.99986V16.2856C3.32129 16.3898 3.36268 16.4897 3.43635 16.5634C3.51003 16.637 3.60995 16.6784 3.71415 16.6784H16.2856C16.3898 16.6784 16.4897 16.637 16.5634 16.5634C16.637 16.4897 16.6784 16.3898 16.6784 16.2856V5.99986C16.6784 5.89567 16.637 5.79574 16.5634 5.72207C16.4897 5.64839 16.3898 5.607 16.2856 5.607H14.7499V7.14272C14.7499 7.55693 14.4141 7.89272 13.9999 7.89272C13.5856 7.89272 13.2499 7.55693 13.2499 7.14272V5.60701H6.74986V7.14272C6.74986 7.55693 6.41407 7.89272 5.99986 7.89272C5.58565 7.89272 5.24986 7.55693 5.24986 7.14272ZM13.4214 9.92231C13.6942 9.61058 13.6626 9.13676 13.3509 8.864C13.0392 8.59124 12.5653 8.62283 12.2926 8.93455L8.75058 12.9825L7.02129 11.6856C6.68992 11.437 6.21982 11.5042 5.97129 11.8356C5.72276 12.1669 5.78992 12.637 6.12129 12.8856L8.407 14.5999C8.72086 14.8353 9.16309 14.789 9.42144 14.4937L13.4214 9.92231Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" class=""></path></svg>
+      My work
+    </RouterLink>
+
     <div class="divider"></div>
-    <RouterLink>Favorites</RouterLink>
+
+    <RouterLink>
+      <span>[star SVG]</span>
+      Favorites
+    </RouterLink>
+
     <div class="divider"></div>
-    <div class="team-btn">My Team</div>
+
+    <div class="workspace-title">
+      <svg viewBox="0 0 20 20" fill="currentColor" width="18" height="18" aria-hidden="true" tabindex="-1" class="icon_a812034417" data-testid="icon"><path d="M3 2.25C2.58579 2.25 2.25 2.58579 2.25 3V8.38462C2.25 8.79883 2.58579 9.13462 3 9.13462H8.38462C8.79883 9.13462 9.13462 8.79883 9.13462 8.38462V3C9.13462 2.58579 8.79883 2.25 8.38462 2.25H3ZM3.75 7.63462V3.75H7.63462V7.63462H3.75ZM11.6154 2.25C11.2012 2.25 10.8654 2.58579 10.8654 3V8.38462C10.8654 8.79883 11.2012 9.13462 11.6154 9.13462H17C17.4142 9.13462 17.75 8.79883 17.75 8.38462V3C17.75 2.58579 17.4142 2.25 17 2.25H11.6154ZM12.3654 7.63462V3.75H16.25V7.63462H12.3654ZM2.25 11.6154C2.25 11.2012 2.58579 10.8654 3 10.8654H8.38462C8.79883 10.8654 9.13462 11.2012 9.13462 11.6154V17C9.13462 17.4142 8.79883 17.75 8.38462 17.75H3C2.58579 17.75 2.25 17.4142 2.25 17V11.6154ZM3.75 12.3654V16.25H7.63462V12.3654H3.75ZM11.6154 10.8654C11.2012 10.8654 10.8654 11.2012 10.8654 11.6154V17C10.8654 17.4142 11.2012 17.75 11.6154 17.75H17C17.4142 17.75 17.75 17.4142 17.75 17V11.6154C17.75 11.2012 17.4142 10.8654 17 10.8654H11.6154ZM12.3654 16.25V12.3654H16.25V16.25H12.3654Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" class="st-current"></path></svg>
+      Workspaces
+    </div>
+
     <div class="search-section flex">
       <div class="search-bar flex align-center">
         <svg viewBox="0 0 20 20" fill="currentColor" width="16px" height="16px" aria-hidden="true" class="icon_a2927e1dcf icon_82075aab38" data-testid="icon"><path d="M8.65191 2.37299C6.9706 2.37299 5.35814 3.04089 4.16927 4.22976C2.9804 5.41863 2.3125 7.03108 2.3125 8.7124C2.3125 10.3937 2.9804 12.0062 4.16927 13.195C5.35814 14.3839 6.9706 15.0518 8.65191 15.0518C10.0813 15.0518 11.4609 14.5691 12.5728 13.6939L16.4086 17.5303C16.7014 17.8232 17.1763 17.8232 17.4692 17.5303C17.7621 17.2375 17.7622 16.7626 17.4693 16.4697L13.6334 12.6333C14.5086 11.5214 14.9913 10.1418 14.9913 8.7124C14.9913 7.03108 14.3234 5.41863 13.1346 4.22976C11.9457 3.04089 10.3332 2.37299 8.65191 2.37299ZM12.091 12.1172C12.9878 11.2113 13.4913 9.98783 13.4913 8.7124C13.4913 7.42891 12.9815 6.19798 12.0739 5.29042C11.1663 4.38285 9.9354 3.87299 8.65191 3.87299C7.36842 3.87299 6.1375 4.38285 5.22993 5.29042C4.32237 6.19798 3.8125 7.42891 3.8125 8.7124C3.8125 9.99589 4.32237 11.2268 5.22993 12.1344C6.1375 13.0419 7.36842 13.5518 8.65191 13.5518C9.92736 13.5518 11.1509 13.0483 12.0568 12.1514C12.0623 12.1455 12.0679 12.1397 12.0737 12.134C12.0794 12.1283 12.0851 12.1227 12.091 12.1172Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
         <input type="text" placeholder="Search" />
         <button title="filters" class=" flex center"><svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16" role="img" aria-hidden="true"><path d="M17.8571 2.87669C18.107 3.41157 18.0246 4.04275 17.6457 4.49555L12.4892 10.6589V15.3856C12.4892 16.0185 12.097 16.5852 11.5048 16.8082L9.56669 17.5381C9.09976 17.7139 8.57627 17.6494 8.16598 17.3655C7.75569 17.0816 7.51084 16.6144 7.51084 16.1155V10.6589L2.35425 4.49555C1.97542 4.04275 1.89302 3.41157 2.14291 2.87669C2.39279 2.34182 2.92977 2 3.52013 2H16.4799C17.0702 2 17.6072 2.34182 17.8571 2.87669ZM16.4799 3.52012H3.52013L8.91611 9.96964C8.99036 10.0584 9.03096 10.1698 9.03096 10.2848V16.1155L10.969 15.3856V10.2848C10.969 10.1698 11.0096 10.0584 11.0839 9.96964L16.4799 3.52012Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></button>
       </div>
-      <button title="Add item to workplace" class="add-btn">+</button>
+      <button title="Add item to workspace" class="add-btn">+</button>
     </div>
-    <Accordion :accordionTitle="'My Team'" class="accordion-container">
-      <div class="accordion-contents flex column">
-        <RouterLink to="">Main</RouterLink>
-        <RouterLink>Sprints</RouterLink>
-        <RouterLink>Epics</RouterLink>
-        <RouterLink>Bug Queue</RouterLink>
-        <RouterLink>Retrospective</RouterLink>
-        <RouterLink>Getting started</RouterLink>
-      </div>
-    </Accordion>
+
+    <section class="workspaces">
+      <RouterLink>
+        [where all workspaces show up in list]
+      </RouterLink>
+    </section>
   </nav>
 </template>
 
 <script>
-import Accordion from './Accordion.vue';
-
 export default {
-  components: {
-    Accordion
-  },
 }
 </script>
 
@@ -48,27 +57,31 @@ nav {
   font-size: 0.875rem;
   gap: 0.2rem;
 
-  a, .team-btn {
+  a,
+  .workspace-title {
+    display: flex;
+    gap: 0.5rem;
     padding: 0.5rem 0.25rem 0.5rem 0.375rem;
     border-radius: 4px;
+  }
 
+  a {
     &:hover {
       background-color: #ebeded;
     }
 
     &.selected {
-      background-color: $green-btns;
+      background-color: #cce5ff;
     }
+  }
+
+  .workspace-title {
+    cursor: default;
   }
 
   .divider {
     height: 1px;
     background-color: $app-gray;
-  }
-
-  .team-btn {
-    font-weight: bold;
-    margin-block: 0.425rem;
   }
 
   .search-section {
@@ -79,10 +92,6 @@ nav {
         background-color: transparent;
         border: 1px $app-gray solid;
         border-radius: 4px;
-
-        &:hover {
-          background-color: #e7ecea;
-        }
       }
 
     .search-bar {
@@ -117,36 +126,6 @@ nav {
     .add-btn {
       height: 2rem;
       width: 2rem;
-    }
-  }
-
-  .accordion-container {
-    height: 100%;
-    ::-webkit-scrollbar {
-        width: 10px;
-    }
-
-    ::-webkit-scrollbar-thumb {
-        border-radius: 4px;
-        background-color: black;
-    }
-
-    ::-webkit-scrollbar-track {
-        border-radius: 4px;
-        background-color: gray;
-    }
-
-    .accordion-contents {
-      padding-inline-start: 1.5rem;
-      overflow-y: auto;
-
-      gap: 0.1rem;
-
-      a {
-        &:hover {
-          background-color: #e7ecea;
-        }
-      }
     }
   }
 }
