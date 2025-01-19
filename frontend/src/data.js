d09456954f00cdd12469bde665e09467079edd55
JennyLottner <jenny.lottner@gmail.com>
2025-01-19 14:40:26 +0200

    Add data file with data structure

---
+++ frontend/src/data.js
@@ -0,0 +1,19 @@
+var board = {
+    title: '',
+    isFavorite: false,
+    groups: [
+        {
+            groupTitle: '',
+            items: [
+                {
+                    itemTitle: '',
+                    updates: [],
+                    people: [],
+                    status: '',
+                    date: new Date(),
+                    labels: [],
+                },
+            ],
+        },
+    ],
+}
