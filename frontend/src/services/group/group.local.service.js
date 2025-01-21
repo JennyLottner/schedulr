3e6f09e0402a75fad50bd10922e638dedc63e1d3
tmanoon <151874385+tmanoon@users.noreply.github.com>
2025-01-21 02:16:08 +0200

    Add basic functions to modify groups in each board in group local service

--- frontend/src/services/group/group.local.service.js
+++ frontend/src/services/group/group.local.service.js
@@ -1,4 +1,6 @@
 import { storageService } from "../storage.service"
+import { boardService } from "../board/board.local.service"
+
 const userBoards = []
 const BOARDS_KEY = 'boards_db'
 const user = userService.getUser()
@@ -13,6 +15,7 @@ const user = userService.getUser()
 //             items: [
 //                 {
 //                     itemTitle: '',
+// id: 1
 //                     updates: [],
 //                     people: [],
 //                     status: '',
@@ -27,22 +30,24 @@ const user = userService.getUser()
 function loadUserBoards() {
     const boards = storageService.loadFromStorage(BOARDS_KEY)
     boards.forEach(board => {
-        if(user.boards.has(board.id)) userBoards.push(board)
+        if (user.boards.has(board.id)) userBoards.push(board)
     })
 }
 
-function _changeTableField(boardId, tableId, field, value) {
+function _changeGroupField(boardId, groupId, field, value) {
     if (!(userBoards(boardId => id === boardId))) return console.error('The user has no access to this board')
-    const boards = _getUserBoards()
-    const chosenBoard = _getBoard(id)
-    const boardToEdit = { ...chosenBoard }
-    boardToEdit[field] = value
-    const idxOfChosenBoard = boards.findIndex(board => id === board.id)
-    boards[idxOfChosenBoard] = boardToEdit
-    saveUpdatedBoards(boards)
+    const boards = boardService.getBoards()
+    const chosenGroup = _getBoardsGroup(boardId, groupId, boards)
+    if (!chosenGroup) return console.error('The user has no access to this group')
+    const groupToEdit = { ...chosenBoard }
+    groupToEdit[field] = value
+    const idxOfChosenGroup = boards.groups.findIndex(group => id === group.id)
+    boardsToUpdate = [ ...boards ]
+    boardsToUpdate.groups[idxOfChosenGroup] = groupToEdit
+    boardService.saveUpdatedBoards(boardsToUpdate)
 }
 
-const _getBoardsTable = (boardId, tableId) => {
-    const chosenBoard = userBoards.find(board => board.id === boardId)
+const _getBoardsGroup = (boardId, groupId, boards) => {
+    const chosenBoard = boards.find(board => board.id === boardId)
     return chosenBoard.groups.find(group => group.id === groupId)
 }
