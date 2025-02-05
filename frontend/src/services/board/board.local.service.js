112c0901c277d1f9e4af2c8f2fd635530c9ed812
tmanoon <151874385+tmanoon@users.noreply.github.com>
2025-02-05 02:53:23 +0200

    Add save add and update functions to board local service

--- frontend/src/services/board/board.local.service.js
+++ frontend/src/services/board/board.local.service.js
@@ -1,5 +1,6 @@
 import { storageService } from "../storage.service"
 import { userService } from "../user/user.local.service"
+import { utilService } from "../util.service"
 
 const BOARDS_KEY = 'boards_db'
 const user = userService.getUser() || null
@@ -47,7 +48,7 @@ const _setUserBoards = () => {
 
 const _getUserBoards = () => userBoards
 
-const query = () => cachedBoards.length > 1 ? cachedBoards : storageService.loadFromStorage(BOARDS_KEY)
+const query = () => cachedBoards.length ? cachedBoards : storageService.loadFromStorage(BOARDS_KEY)
 
 const saveUpdatedBoards = (boards) => storageService.saveToStorage(BOARDS_KEY, boards)
 
@@ -55,26 +56,26 @@ const createDefaultBoard = () => defaultBoard
 
 const getBoard = (id) => storageService.loadFromStorage(BOARDS_KEY).find(board => id === board.id)
 
-function changeBoardField(field, id, value) {
-    if (!(userBoards(boardId => id === boardId))) return console.error('The user has no access to this board')
-    const boards = _getUserBoards()
-    const chosenBoard = getBoard(id)
-    const boardToEdit = { ...chosenBoard }
-    boardToEdit[field] = value
-    const idxOfChosenBoard = boards.findIndex(board => id === board.id)
+function updateBoard(board) {
+    const boards = query()
+    const chosenBoard = boards.find(_board => _board.id === board.id)
+    if (chosenBoard.members.find(member => member.id === user.id)) return console.error('The user has no access to this board')
+    const idxOfChosenBoard = boards.findIndex(_board => _board.id === board.id)
     boards[idxOfChosenBoard] = boardToEdit
     saveUpdatedBoards(boards)
 }
 
-function changeBoardTitle(id, title) {
-    changeBoardField('title', id, title)
+function addBoard(board) {
+    const boards = query()
+    if (board.members.find(member => member.id === user.id)) return console.error('The user has no access to this board')
+    board.id = utilService.makeId()
+    boards.push(board)
+    saveUpdatedBoards(boards)
 }
 
 function toggleBoardToUserFavoriteBoards(id) {
     const userBoards = user.boards
-    userBoards[id] ?
-        userBoards[id].set(false) :
-        favoriteBoards.set(true)
+    userBoards.get(id) ? userBoards[id].set(false) : favoriteBoards.set(true)
 }
 
 const addMemberToBoard = (boardId, userId) => {
@@ -107,9 +108,14 @@ const addGroup = (boardId) => {
     const boards = query()
     const board = getBoard(boardId)
     const chosenBoardIdx = boards.findIndex(board => board.id === boardId)
-    const boardsToUpdate = [ ...boards ]
+    const boardsToUpdate = [...boards]
     const group = _createGroup()
     board.groups.push(group)
     boardsToUpdate[chosenBoardIdx] = board
     saveUpdatedBoards(boardsToUpdate)
+}
+
+function save(editedBoard) {
+    if (editedBoard.id) updateBoard(editedBoard)
+    else addBoard(editedBoard)
 }
