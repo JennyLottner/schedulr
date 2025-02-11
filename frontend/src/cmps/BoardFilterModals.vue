efabe0369ee7f68a1c018db28cbd902982b36b83
JennyLottner <jenny.lottner@gmail.com>
2025-02-11 12:22:53 +0200

    Reorder styling in filter modals

--- frontend/src/cmps/BoardFilterModals.vue
+++ frontend/src/cmps/BoardFilterModals.vue
@@ -2,47 +2,11 @@
   <div class="modal">
     <article v-if="selected === 'more'" class="more-modal">
       <button class="more-btn grid align-center">
-        <svg
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          width="16"
-          height="16"
-          role="img"
-          tabindex="0"
-          aria-hidden="false"
-          aria-label="New group of items"
-          class="icon_35c1b9ef14 icon-service-icon noFocusStyle_e846aee9b1"
-          data-testid="icon"
-        >
-          <path
-            d="M16 4.5H7.5L7.5 15.5H16C16.2761 15.5 16.5 15.2761 16.5 15V5C16.5 4.72386 16.2761 4.5 16 4.5ZM4 4.5H6L6 15.5H4C3.72386 15.5 3.5 15.2761 3.5 15V5C3.5 4.72386 3.72386 4.5 4 4.5ZM4 3C2.89543 3 2 3.89543 2 5V15C2 16.1046 2.89543 17 4 17H16C17.1046 17 18 16.1046 18 15V5C18 3.89543 17.1046 3 16 3H4ZM15 14V9H9V14H15Z"
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16" role="img" tabindex="0" aria-hidden="false" aria-label="New group of items" class="icon_35c1b9ef14 icon-service-icon noFocusStyle_e846aee9b1" data-testid="icon" ><path d="M16 4.5H7.5L7.5 15.5H16C16.2761 15.5 16.5 15.2761 16.5 15V5C16.5 4.72386 16.2761 4.5 16 4.5ZM4 4.5H6L6 15.5H4C3.72386 15.5 3.5 15.2761 3.5 15V5C3.5 4.72386 3.72386 4.5 4 4.5ZM4 3C2.89543 3 2 3.89543 2 5V15C2 16.1046 2.89543 17 4 17H16C17.1046 17 18 16.1046 18 15V5C18 3.89543 17.1046 3 16 3H4ZM15 14V9H9V14H15Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
         New item
       </button>
       <button class="more-btn grid align-center">
-        <svg
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          width="16"
-          height="16"
-          role="img"
-          tabindex="0"
-          aria-hidden="false"
-          aria-label="New group of items"
-          class="icon_35c1b9ef14 icon-service-icon noFocusStyle_e846aee9b1"
-          data-testid="icon"
-        >
-          <path
-            d="M16 4.5H7.5L7.5 15.5H16C16.2761 15.5 16.5 15.2761 16.5 15V5C16.5 4.72386 16.2761 4.5 16 4.5ZM4 4.5H6L6 15.5H4C3.72386 15.5 3.5 15.2761 3.5 15V5C3.5 4.72386 3.72386 4.5 4 4.5ZM4 3C2.89543 3 2 3.89543 2 5V15C2 16.1046 2.89543 17 4 17H16C17.1046 17 18 16.1046 18 15V5C18 3.89543 17.1046 3 16 3H4ZM15 14V9H9V14H15Z"
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16" role="img" tabindex="0" aria-hidden="false" aria-label="New group of items" class="icon_35c1b9ef14 icon-service-icon noFocusStyle_e846aee9b1" data-testid="icon"><path d="M16 4.5H7.5L7.5 15.5H16C16.2761 15.5 16.5 15.2761 16.5 15V5C16.5 4.72386 16.2761 4.5 16 4.5ZM4 4.5H6L6 15.5H4C3.72386 15.5 3.5 15.2761 3.5 15V5C3.5 4.72386 3.72386 4.5 4 4.5ZM4 3C2.89543 3 2 3.89543 2 5V15C2 16.1046 2.89543 17 4 17H16C17.1046 17 18 16.1046 18 15V5C18 3.89543 17.1046 3 16 3H4ZM15 14V9H9V14H15Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
         New group of items
       </button>
     </article>
@@ -50,28 +14,11 @@
     <article v-else-if="selected === 'search'" class="search-modal">
       <form>
         <div class="search-bar grid align-center">
-          <svg
-            viewBox="0 0 20 20"
-            fill="#68697a"
-            width="16px"
-            height="16px"
-            aria-hidden="true"
-            class="icon_a2927e1dcf icon_82075aab38"
-            data-testid="icon"
-          >
-            <path
-              d="M8.65191 2.37299C6.9706 2.37299 5.35814 3.04089 4.16927 4.22976C2.9804 5.41863 2.3125 7.03108 2.3125 8.7124C2.3125 10.3937 2.9804 12.0062 4.16927 13.195C5.35814 14.3839 6.9706 15.0518 8.65191 15.0518C10.0813 15.0518 11.4609 14.5691 12.5728 13.6939L16.4086 17.5303C16.7014 17.8232 17.1763 17.8232 17.4692 17.5303C17.7621 17.2375 17.7622 16.7626 17.4693 16.4697L13.6334 12.6333C14.5086 11.5214 14.9913 10.1418 14.9913 8.7124C14.9913 7.03108 14.3234 5.41863 13.1346 4.22976C11.9457 3.04089 10.3332 2.37299 8.65191 2.37299ZM12.091 12.1172C12.9878 11.2113 13.4913 9.98783 13.4913 8.7124C13.4913 7.42891 12.9815 6.19798 12.0739 5.29042C11.1663 4.38285 9.9354 3.87299 8.65191 3.87299C7.36842 3.87299 6.1375 4.38285 5.22993 5.29042C4.32237 6.19798 3.8125 7.42891 3.8125 8.7124C3.8125 9.99589 4.32237 11.2268 5.22993 12.1344C6.1375 13.0419 7.36842 13.5518 8.65191 13.5518C9.92736 13.5518 11.1509 13.0483 12.0568 12.1514C12.0623 12.1455 12.0679 12.1397 12.0737 12.134C12.0794 12.1283 12.0851 12.1227 12.091 12.1172Z"
-              fill="#68697a"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <input
-            type="text"
+          <svg viewBox="0 0 20 20" fill="#68697a" width="16px" height="16px" aria-hidden="true" class="icon_a2927e1dcf icon_82075aab38" data-testid="icon">  <path    d="M8.65191 2.37299C6.9706 2.37299 5.35814 3.04089 4.16927 4.22976C2.9804 5.41863 2.3125 7.03108 2.3125 8.7124C2.3125 10.3937 2.9804 12.0062 4.16927 13.195C5.35814 14.3839 6.9706 15.0518 8.65191 15.0518C10.0813 15.0518 11.4609 14.5691 12.5728 13.6939L16.4086 17.5303C16.7014 17.8232 17.1763 17.8232 17.4692 17.5303C17.7621 17.2375 17.7622 16.7626 17.4693 16.4697L13.6334 12.6333C14.5086 11.5214 14.9913 10.1418 14.9913 8.7124C14.9913 7.03108 14.3234 5.41863 13.1346 4.22976C11.9457 3.04089 10.3332 2.37299 8.65191 2.37299ZM12.091 12.1172C12.9878 11.2113 13.4913 9.98783 13.4913 8.7124C13.4913 7.42891 12.9815 6.19798 12.0739 5.29042C11.1663 4.38285 9.9354 3.87299 8.65191 3.87299C7.36842 3.87299 6.1375 4.38285 5.22993 5.29042C4.32237 6.19798 3.8125 7.42891 3.8125 8.7124C3.8125 9.99589 4.32237 11.2268 5.22993 12.1344C6.1375 13.0419 7.36842 13.5518 8.65191 13.5518C9.92736 13.5518 11.1509 13.0483 12.0568 12.1514C12.0623 12.1455 12.0679 12.1397 12.0737 12.134C12.0794 12.1283 12.0851 12.1227 12.091 12.1172Z"    fill="#68697a"    fill-rule="evenodd"    clip-rule="evenodd"  ></path></svg>
+          <input type="text"
             :value="filterBy.txt"
             @input="updateFilter('txt', $event.target.value)"
-            placeholder="Search this board"
-          />
+            placeholder="Search this board"/>
         </div>
       </form>
     </article>
@@ -84,32 +31,13 @@
           <li v-for="user in users" :key="user.id">
             <button
               @click="updateFilter('person', user.id)"
-              :title="user.fullName"
-            >
+              :title="user.fullName">
               <img :src="user.imgUrl" :alt="user.fullName" />
             </button>
           </li>
           <li>
-            <button
-              @click="updateFilter('person', '')"
-              title="all users"
-              class="flex center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                fill="#000000"
-                height="20px"
-                width="20px"
-                version="1.1"
-                id="Layer_1"
-                viewBox="0 0 1792 1792"
-                xml:space="preserve"
-              >
-                <path
-                  d="M1082.2,896.6l410.2-410c51.5-51.5,51.5-134.6,0-186.1s-134.6-51.5-186.1,0l-410.2,410L486,300.4  c-51.5-51.5-134.6-51.5-186.1,0s-51.5,134.6,0,186.1l410.2,410l-410.2,410c-51.5,51.5-51.5,134.6,0,186.1  c51.6,51.5,135,51.5,186.1,0l410.2-410l410.2,410c51.5,51.5,134.6,51.5,186.1,0c51.1-51.5,51.1-134.6-0.5-186.2L1082.2,896.6z"
-                />
-              </svg>
+            <button @click="updateFilter('person', '')" title="all users" class="flex center">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 1792 1792" xml:space="preserve">  <path    d="M1082.2,896.6l410.2-410c51.5-51.5,51.5-134.6,0-186.1s-134.6-51.5-186.1,0l-410.2,410L486,300.4  c-51.5-51.5-134.6-51.5-186.1,0s-51.5,134.6,0,186.1l410.2,410l-410.2,410c-51.5,51.5-51.5,134.6,0,186.1  c51.6,51.5,135,51.5,186.1,0l410.2-410l410.2,410c51.5,51.5,134.6,51.5,186.1,0c51.1-51.5,51.1-134.6-0.5-186.2L1082.2,896.6z"  /></svg>
             </button>
           </li>
         </ul>
@@ -259,13 +187,14 @@ article {
 
   &.person-modal {
     max-width: 20rem;
+
+    gap: 0.5rem;
+
     scrollbar-width: none;
 
     ::-webkit-scrollbar {
       display: none;
     }
-
-    gap: 0.5rem;
 
     ul {
       padding: 0.5rem 0.2rem;
