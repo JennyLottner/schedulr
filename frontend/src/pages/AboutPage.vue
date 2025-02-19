b06c6d228947d1186f27237b75b5ea25e3acba4a
JennyLottner <jenny.lottner@gmail.com>
2025-02-19 21:17:48 +0200

    Create about page

---
+++ frontend/src/pages/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="about-page">
+    <header>
+        <h1>About</h1>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "../style/setup/vars";
+
+</style>
