c87a12954371bc759f159ea56d6d966650adc1bc
JennyLottner <jenny.lottner@gmail.com>
2025-02-19 21:00:10 +0200

    Modify user image in header

--- frontend/src/cmps/AppHeader.vue
+++ frontend/src/cmps/AppHeader.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="app-header flex align-center space-between">
+  <div class="app-header flex align-center">
+    <header class="flex align-center space-between">
     <section class="logo-section flex align-center">
       <button class="nav-btn" @click="$emit('toggleNavBar')">
         <svg  viewBox="0 0 20 20"  fill="currentColor"  width="18"  height="18"  aria-hidden="true"  class="icon_f74f57d4ab"  data-testid="icon">  <g clip-path="url(#a)">    <path      d="M4.5 2.25a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm7.75 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm7.75 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0ZM4.5 10A2.25 2.25 0 1 1 0 10a2.25 2.25 0 0 1 4.5 0Zm7.75 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0ZM20 10a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0ZM4.5 17.75a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm7.75 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm7.75 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"    ></path>  </g>  <defs>    <clipPath id="a"><path d="M0 0h20v20H0z"></path></clipPath>  </defs></svg>
@@ -32,6 +33,7 @@
           <svg  v-else  xmlns="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink"  version="1.1"  width="256"  height="256"  viewBox="0 0 256 256"  xml:space="preserve">  <defs></defs>  <g    style="      stroke: none;      stroke-width: 0;      stroke-dasharray: none;      stroke-linecap: butt;      stroke-linejoin: miter;      stroke-miterlimit: 10;      fill: none;      fill-rule: nonzero;      opacity: 1;    "    transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"  >    <path      d="M 45 88 c -11.049 0 -21.18 -2.003 -29.021 -8.634 C 6.212 71.105 0 58.764 0 45 C 0 20.187 20.187 0 45 0 c 24.813 0 45 20.187 45 45 c 0 13.765 -6.212 26.105 -15.979 34.366 C 66.181 85.998 56.049 88 45 88 z"      style="        stroke: none;        stroke-width: 1;        stroke-dasharray: none;        stroke-linecap: butt;        stroke-linejoin: miter;        stroke-miterlimit: 10;        fill: rgb(214, 214, 214);        fill-rule: nonzero;        opacity: 1;      "      transform=" matrix(1 0 0 1 0 0) "      stroke-linecap="round"    />    <path      d="M 45 60.71 c -11.479 0 -20.818 -9.339 -20.818 -20.817 c 0 -11.479 9.339 -20.818 20.818 -20.818 c 11.479 0 20.817 9.339 20.817 20.818 C 65.817 51.371 56.479 60.71 45 60.71 z"      style="        stroke: none;        stroke-width: 1;        stroke-dasharray: none;        stroke-linecap: butt;        stroke-linejoin: miter;        stroke-miterlimit: 10;        fill: rgb(165, 164, 164);        fill-rule: nonzero;        opacity: 1;      "      transform=" matrix(1 0 0 1 0 0) "      stroke-linecap="round"    />    <path      d="M 45 90 c -10.613 0 -20.922 -3.773 -29.028 -10.625 c -0.648 -0.548 -0.88 -1.444 -0.579 -2.237 C 20.034 64.919 31.933 56.71 45 56.71 s 24.966 8.209 29.607 20.428 c 0.301 0.793 0.069 1.689 -0.579 2.237 C 65.922 86.227 55.613 90 45 90 z"      style="        stroke: none;        stroke-width: 1;        stroke-dasharray: none;        stroke-linecap: butt;        stroke-linejoin: miter;        stroke-miterlimit: 10;        fill: rgb(165, 164, 164);        fill-rule: nonzero;        opacity: 1;      "      transform=" matrix(1 0 0 1 0 0) "      stroke-linecap="round"    />  </g></svg>
         </div>
       </button>
+
     </section>
   </header>
 </template>
@@ -40,7 +42,7 @@
 export default {
   computed: {
     user() {
-      return this.$store.getters.user;
+      return this.$store.getters.user
     },
   },
 }
@@ -51,9 +53,13 @@ export default {
 @import "../style/setup/vars";
 
 .app-header {
+  width: 100vw;
   padding-inline: 0.75rem;
   background-color: transparent;
-  width: 100vw;
+
+  header {
+    width: 100%;
+  }
 
   button {
     @include headerBtn;
@@ -111,13 +117,17 @@ export default {
     }
 
     .user-img {
+      width: 2rem;
+      height: 2rem;
       transform: translateX(7px) translateY(1px);
       border-radius: 50%;
+      background-color: $primary-selected-color;
 
       svg,
       img {
         width: 2rem;
         height: 2rem;
+        border-radius: 50%;
       }
 
       img {
